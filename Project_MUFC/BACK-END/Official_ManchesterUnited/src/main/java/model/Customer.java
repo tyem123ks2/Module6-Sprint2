5a36947166f8fcb10a9efa7d709851b01e4c7607
tyem123ks2 <thanhtuan02051011@gmail.com>
2023-06-12 16:26:07 +0700

    first commit

---
+++ Project_MUFC/BACK-END/Official_ManchesterUnited/src/main/java/model/Customer.java
@@ -0,0 +1,51 @@
+package model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String customerCode;
+    private String name;
+    private String phoneNumber;
+
+    public Customer() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCustomerCode() {
+        return customerCode;
+    }
+
+    public void setCustomerCode(String customerCode) {
+        this.customerCode = customerCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
---
+++ Project_MUFC/BACK-END/Official_ManchesterUnited/src/test/java/com/example/official_manchesterunited/OfficialManchesterUnitedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.official_manchesterunited;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficialManchesterUnitedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
